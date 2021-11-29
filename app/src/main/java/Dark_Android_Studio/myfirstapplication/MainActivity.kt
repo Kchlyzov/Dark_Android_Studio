7021e7847d17f75910e90619ed0f4497b319a669
Анатолий <95156389+Kchlyzov@users.noreply.github.com>
2021-11-29 13:54:57 +0600

    first commit

---
+++ app/src/main/java/Dark_Android_Studio/myfirstapplication/MainActivity.kt
@@ -0,0 +1,11 @@
+package Dark_Android_Studio.myfirstapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
